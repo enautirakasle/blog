b66343a5b51527dccd77cee456fa7c212ce48298
Eñaut Agirre <enaut.irakasle@gmail.com>
2019-02-11 13:38:20 +0100

    lehen commita

---
+++ src/myfilter/AdminFilter.java
@@ -0,0 +1,59 @@
+package myfilter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class AdminFilter
+ */
+@WebFilter("/AdminFilter")
+public class AdminFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public AdminFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest)request;
+		HttpSession session = req.getSession();
+		
+		if(session.getAttribute("usuarioLogueado")==null) {//session on iniciada && !req.getRequestURI().endsWith("admin/login")
+			HttpServletResponse resp = (HttpServletResponse)response;
+			resp.sendRedirect(req.getContextPath()+"/login");
+		}else {
+			chain.doFilter(request, response);
+		}
+		
+		
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
